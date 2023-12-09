92ccd56977e1e4512f966af184b477089767e71f
Bezes13 <AlexBelzer98@gmail.com>
2023-12-09 12:24:51 +0100

    Working Prototype

--- app/src/main/java/com/example/simpletodo/MainScreen.kt
+++ app/src/main/java/com/example/simpletodo/MainScreen.kt
@@ -1,9 +1,20 @@
 package com.example.simpletodo
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.SmallFloatingActionButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -11,34 +22,118 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shadow
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.simpletodo.ui.theme.PurpleGrey80
 
 @Composable
 fun MainScreen(
     mainViewModel: MainViewModel
 ) {
-    var newTodoText by remember { mutableStateOf("") }
-
     val retrievedList by mainViewModel.retrievedList.observeAsState(initial = emptyList())
+    val doneList by mainViewModel.doneList.observeAsState(initial = emptyList())
+    MainScreen(mainViewModel::updateList, retrievedList, doneList, mainViewModel::markAsDone)
+}
 
+@Composable
+fun MainScreen(
+    updateList: (String, Boolean) -> Unit,
+    todoList: List<String>,
+    doneList: List<String>,
+    markAsDone: (String) -> Unit
+) {
+    var newTodoText by remember { mutableStateOf("") }
     Column {
-        Text(text = stringResource(id = R.string.Header))
-        retrievedList.forEach {
-            Row {
-                Text(text = it)
-                Button(onClick = { mainViewModel.updateList(it, false) }) {
-
+        Text(
+            text = stringResource(id = R.string.Header),
+            fontSize = 50.sp,
+            style = TextStyle(
+                color = Color.Black, shadow = Shadow(
+                    PurpleGrey80, Offset(2f, 2f)
+                ),
+                textDecoration = TextDecoration.Underline
+            ),
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        )
+        todoList.forEach {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(80.dp)
+                ) {
+                    Box(modifier = Modifier
+                        .weight(1f)
+                        .align(Alignment.CenterVertically)
+                        .fillMaxHeight()){
+                        Row (
+                            Modifier
+                                .padding(horizontal = 10.dp)
+                                .fillMaxHeight()){
+                            Text(
+                                text = it,
+                                style = TextStyle(
+                                    textDecoration = if (doneList.contains(it)) TextDecoration.LineThrough else TextDecoration.None
+                                ),
+                                modifier = Modifier.align(Alignment.CenterVertically) // Takes up available space
+                            )
+                            SmallFloatingActionButton(
+                                modifier = Modifier
+                                    .padding(horizontal = 10.dp)
+                                    .size(20.dp)
+                                    .align(Alignment.CenterVertically),
+                                onClick = { markAsDone(it) },
+                                containerColor = Color.Gray
+                            ) {
+                                Icon(Icons.Filled.Check, contentDescription = "Mark as Done")
+                            }
+                        }
+                    }
+                    SmallFloatingActionButton(
+                        onClick = { updateList(it, false) },
+                        containerColor = Color.Red,
+                        modifier = Modifier
+                            .align(Alignment.CenterVertically)
+                    ) {
+                        Icon(Icons.Filled.Clear, "Delete Todo")
+                    }
                 }
             }
-        }
-        OutlinedTextField(
-            value = newTodoText,
-            onValueChange = { newTodoText = it },
-            label = { Text(stringResource(id = R.string.newTodo)) }
-        )
-        Button(onClick = { mainViewModel.updateList(newTodoText, true) }) {
 
+        }
+
+        Row (
+            Modifier
+                .fillMaxWidth()
+                .height(50.dp)){
+            OutlinedTextField(
+                value = newTodoText,
+                onValueChange = { newTodoText = it },
+                label = { Text(stringResource(id = R.string.newTodo)) },
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .align(Alignment.CenterVertically)
+            )
+            SmallFloatingActionButton(
+                onClick = { updateList(newTodoText, true) },
+                containerColor = Color.Green,
+                contentColor = Color.Black,
+                modifier = Modifier.fillMaxHeight()
+            ) {
+                Icon(Icons.Filled.Add, "Add new TODO")
+            }
         }
     }
 }
@@ -46,5 +141,8 @@ fun MainScreen(
 @Composable
 @Preview
 fun PreviewMainScreen() {
-
+    MainScreen({ _, _ -> },
+        listOf("Koks essen", "Kokain Bär sehen", "Apps Coden"),
+        listOf("Kokain Bär sehen"),
+        { _ -> })
 }
--- app/src/main/java/com/example/simpletodo/MainViewModel.kt
+++ app/src/main/java/com/example/simpletodo/MainViewModel.kt
@@ -6,30 +6,49 @@ import androidx.lifecycle.ViewModel
 
 class MainViewModel(private var sharedPreferencesManager: SharedPreferencesManager) : ViewModel() {
     private val _retrievedList = MutableLiveData<List<String>>()
+    private val _doneList = MutableLiveData<List<String>>()
 
     val retrievedList: LiveData<List<String>> get() = _retrievedList
+    val doneList: LiveData<List<String>> get() = _doneList
 
     init {
         _retrievedList.value =
             sharedPreferencesManager.getList(sharedPreferencesManager.context.getString(R.string.todoList))
+        _doneList.value = sharedPreferencesManager.getList(sharedPreferencesManager.context.getString(R.string.doneList))
+    }
+
+    fun markAsDone(text: String){
+        val currentList = _doneList.value?.toMutableList() ?: mutableListOf()
+
+        if(currentList.contains(text)){
+            currentList.remove(text)
+        }else{
+            currentList.add(text)
+        }
+        _doneList.value = currentList
+        saveList(currentList, R.string.doneList)
     }
 
     fun updateList(text: String, addItem: Boolean) {
         val currentList = _retrievedList.value?.toMutableList() ?: mutableListOf()
+        val doneList = _doneList.value?.toMutableList() ?: mutableListOf()
 
         if (addItem) {
             currentList.add(text)
         } else {
             currentList.remove(text)
+            if(doneList.contains(text))
+                doneList.remove(text)
         }
-
+        _doneList.value = doneList
         _retrievedList.value = currentList
-        saveList(currentList)
+        saveList(currentList, R.string.todoList)
+        saveList(doneList, R.string.doneList)
     }
 
-    private fun saveList(myList: List<String>) {
+    private fun saveList(myList: List<String>, listID: Int) {
         sharedPreferencesManager.saveList(
-            sharedPreferencesManager.context.getString(R.string.todoList),
+            sharedPreferencesManager.context.getString(listID),
             myList
         )
     }
